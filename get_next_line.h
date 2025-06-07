01fc4137072b203eb1fee95b3a2edf0ffa3062b0
Bavithran Pichyalagan <bpichyal@u91z04s06.42kl.edu.my>
2025-06-07 17:37:13 +0800

    sikit

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpichyal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 12:20:13 by bpichyal          #+#    #+#             */
+/*   Updated: 2025/06/07 12:30:55 by bpichyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+/* a standard buffer size if in case its not defined in terminal..*/
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+/*will add the functions after i finsh my functions la*/
+
+#endif
