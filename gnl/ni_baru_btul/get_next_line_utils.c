b6275e4dfd22982cee9151d8d2d86d807418ce85
Bavithran Pichyalagan <bpichyal@u90z02s01.42kl.edu.my>
2025-06-12 18:14:26 +0800

    i actually added many just so far ok la

---
+++ gnl/ni_baru_btul/get_next_line_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpichyal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 16:24:28 by bpichyal          #+#    #+#             */
+/*   Updated: 2025/06/12 16:26:09 by bpichyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#inlcude "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while(*s)
+	{
+		while (*(unsigned char *)s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*(unsigned char *)s == (unsigned char)c)
+		returm ((char *)buffer);
+	return (NULL);
+
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		len;
+
+	len = 0;
+	while (src[len])
+		len++;
+	dest = (char *) malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	new = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new)
+		return (NULL);
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		new[i] = s2[j];
+		i++;
+		j++;
+	}
+	new[i] = '\0';
+	if (s1)
+		free(s1)
+	return (new);
+}
